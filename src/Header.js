29594b8ecc2638064e1ef1bce9f0412636e4b1b1
AndYuz27 <yuzlov2003@gmail.com>
2023-03-01 12:55:09 +0300

    first commit

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+
+
+export default function Header() {
+    
+
+
+    return(
+        <div className="hdr_main">
+            <div className="logo">AndY-Telecom</div>
+            <div className="nav_aaa">
+                <a href="#" className="lnk1">Link 1</a>
+                <a href="#" className="lnk1">Link 2</a>
+                <a href="#" className="lnk1">Link 3</a>
+                <a href="#" className="lnk1">Link 4</a>
+                <a href="#" className="lnk2">The Best Link</a>
+            </div>
+        </div>
+    )
+}
